2bdd2a9de44c5294c58fe9418d89d34a45b750ac
learnman18 <manishpan18@gmail.com>
2023-11-20 13:03:25 +0530

    AllFiles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+// import { ReactDOM } from 'react';
 import './App.css';
+import Increment from './components/Increment';
+// import ToDoList from './components/ToDoList';
+// import FormTwo from './components/FormTwo';
+// import FormOne from './components/FormOne';
+// import DigitalClock from './components/DigitalClock';
+// import ExactTime from './components/StateHook';
+// import SlotMatch from './components/SlotMatch';
+// import Cards from './components/MyCards';
+// import Greets from './components/greets';
+// import MiniCalculation, {Addition, Multiply, Subtract, Divide} from './components/MiniCal';
+// import Mov from './components/MovieData';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Increment></Increment>
+    </>
   );
 }
 
